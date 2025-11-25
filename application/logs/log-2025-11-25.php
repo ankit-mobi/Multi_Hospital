328b40bfaf3f35dca76d9fb4e563c4396baa98bf
baghe <baghelankit602@gmail.com>
2025-11-25 10:04:40 +0530

    update steps from 1 to 5

---
+++ application/logs/log-2025-11-25.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-11-25 05:17:51 --> Severity: error --> Exception: Configured database connection is persistent. Aborting. D:\xampp\htdocs\Multi-Hospital\system\libraries\Session\drivers\Session_database_driver.php 95
+ERROR - 2025-11-25 05:18:31 --> Severity: error --> Exception: Configured database connection is persistent. Aborting. D:\xampp\htdocs\Multi-Hospital\system\libraries\Session\drivers\Session_database_driver.php 95
